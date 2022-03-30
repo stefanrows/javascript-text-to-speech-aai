9f73b09bf5f417914aafaa22e9f225e1ecda0a99
Stefan Rows <38349705+stefanrows@users.noreply.github.com>
2022-03-30 08:54:46 +0200

    Initial

---
+++ script.js
@@ -0,0 +1,21 @@
+const textInputField = document.querySelector("#text-input")
+const form = document.querySelector("#form")
+const utterThis = new SpeechSynthesisUtterance()
+const synth = window.speechSynthesis
+let ourText = ""
+
+const checkBrowserCompatibility = () => {
+  "speechSynthesis" in window
+    ? console.log("Web Speech API supported!")
+    : console.log("Web Speech API not supported :-(")
+}
+
+checkBrowserCompatibility()
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+  ourText = textInputField.value
+  utterThis.text = ourText
+  synth.speak(utterThis)
+  textInputField.value = ""
+}
